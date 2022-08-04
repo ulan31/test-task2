921e82e8e1d41bc9847b28e8ab56b5ac2c0f943d
ulan <ulan.aliekov@gmail.com>
2022-08-04 12:47:53 +0300

    init form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Autocomplete from '@mui/material/Autocomplete';
+import {logDOM} from "@testing-library/react";
+
+const topFilms = [
+    { label: 'ulan' },
+    { label: 'ulan1' },
+    { label: 'ulan2' },
+]
+
+const change = (e) => console.log(e)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <form>
+          <Box>
+              <Grid container>
+                  <Grid item xs={12} md={6} lg={4}>
+                      <Paper>
+                          <Autocomplete
+                              id='ttt'
+                              options={topFilms}
+                              onOpen={change}
+                              freeSolo
+                              renderInput={(params) => <TextField {...params}  id="outlined-basic" label="Outlined" variant="outlined" />}
+                          />
+                      </Paper>
+                  </Grid>
+                  <Grid item xs={12} md={6} lg={4}>
+                      <Paper>
+                          <Button type={"submit"} variant="contained">Submit</Button>
+                      </Paper>
+                  </Grid>
+              </Grid>
+          </Box>
+      </form>
+  )
 }
 
 export default App;
